9037e20eb5e8ea7937ffde29bfa6478d98fd9da0
A-nk-ita <ankibisht1996@gmail.com>
2020-05-10 14:46:00 +0530

    add

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -16,7 +16,7 @@ public class Calculator {
         double number1, number2;
         do {
             System.out.println("Calculator-DevOps, Choose to perform operation");
-            System.out.println("Press 1 to Add\n" +
+            System.out.println("Press 1 to Add\nPress 2 to Subtract\nPress 3 to Multiply\nPress 4 to Divide\n" +
                     "Press any other key to exit\nEnter your choice: ");
             int choice = scanner.nextInt();
             switch (choice) {
@@ -27,30 +27,30 @@ public class Calculator {
                     number2 = scanner.nextDouble();
                     System.out.println("Addition result is : " + calculator.add(number1, number2));
                     break;
-//                case 2:
-//                    // do subtraction
-//                    System.out.print("Enter the first number : ");
-//                    number1 = scanner.nextDouble();
-//                    System.out.print("Enter the second number : ");
-//                    number2 = scanner.nextDouble();
-//                    System.out.println("Subtraction result is : " + calculator.subtract(number1, number2));
-//                    break;
-//                case 3:
-//                    // do multiplication
-//                    System.out.print("Enter the first number : ");
-//                    number1 = scanner.nextDouble();
-//                    System.out.print("Enter the second number : ");
-//                    number2 = scanner.nextDouble();
-//                    System.out.println("Multiplication result is : " + calculator.multiply(number1, number2));
-//                    break;
-//                case 4:
-//                    // do division
-//                    System.out.print("Enter the first number : ");
-//                    number1 = scanner.nextDouble();
-//                    System.out.print("Enter the second number : ");
-//                    number2 = scanner.nextDouble();
-//                    System.out.println("Division result is : " + calculator.divide(number1, number2));
-//                    break;
+                case 2:
+                    // do subtraction
+                    System.out.print("Enter the first number : ");
+                    number1 = scanner.nextDouble();
+                    System.out.print("Enter the second number : ");
+                    number2 = scanner.nextDouble();
+                    System.out.println("Subtraction result is : " + calculator.subtract(number1, number2));
+                    break;
+                case 3:
+                    // do multiplication
+                    System.out.print("Enter the first number : ");
+                    number1 = scanner.nextDouble();
+                    System.out.print("Enter the second number : ");
+                    number2 = scanner.nextDouble();
+                    System.out.println("Multiplication result is : " + calculator.multiply(number1, number2));
+                    break;
+                case 4:
+                    // do division
+                    System.out.print("Enter the first number : ");
+                    number1 = scanner.nextDouble();
+                    System.out.print("Enter the second number : ");
+                    number2 = scanner.nextDouble();
+                    System.out.println("Division result is : " + calculator.divide(number1, number2));
+                    break;
                 default:
                     System.out.println("Exiting....");
                     return;
@@ -66,42 +66,42 @@ public class Calculator {
         return result;
     }
 
-//    public double subtract(double number1, double number2) {
-//        logger.info("Subtracting two numbers " + number1 + " and " + number2);
-//        double result = number1 - number2;
-//        logger.info("Result of subtraction is " + result);
-//        return result;
-//    }
-//
-//
-//    public double multiply(double number1, double number2) {
-//        logger.info("Multiplying two numbers " + number1 + " and " + number2);
-//        double result = number1 * number2;
-//        logger.info("Result of multiplication is " + result);
-//        return result;
-//    }
-//
-//    public double divide(double number1, double number2) {
-//        double result = 0;
-//        try {
-//            logger.info("Dividing two numbers " + number1 + " and " + number2);
-//            if (number1 == 0 && number2 == 0) {
-//                result = Double.NaN;
-//                throw new ArithmeticException("Case of NaN 0.0/0.0");
-//            } else if (number1 > 0 && number2 == 0) {
-//                result = Double.POSITIVE_INFINITY;
-//                throw new ArithmeticException("Case of Positive Infinity 1.0/0.0");
-//            } else if (number1 <= -1 && number2 == 0) {
-//                result = Double.NEGATIVE_INFINITY;
-//                throw new ArithmeticException("Case of Negative Infinity -1.0/0.0");
-//            } else {
-//                result = number1 / number2;
-//            }
-//        } catch (ArithmeticException error) {
-//            logger.error("Number cannot be divided by zero " + error.getLocalizedMessage());
-//        } finally {
-//            logger.info("Result of dividing is " + result);
-//        }
-//        return result;
-//    }
+    public double subtract(double number1, double number2) {
+        logger.info("Subtracting two numbers " + number1 + " and " + number2);
+        double result = number1 - number2;
+        logger.info("Result of subtraction is " + result);
+        return result;
+    }
+
+
+    public double multiply(double number1, double number2) {
+        logger.info("Multiplying two numbers " + number1 + " and " + number2);
+        double result = number1 * number2;
+        logger.info("Result of multiplication is " + result);
+        return result;
+    }
+
+    public double divide(double number1, double number2) {
+        double result = 0;
+        try {
+            logger.info("Dividing two numbers " + number1 + " and " + number2);
+            if (number1 == 0 && number2 == 0) {
+                result = Double.NaN;
+                throw new ArithmeticException("Case of NaN 0.0/0.0");
+            } else if (number1 > 0 && number2 == 0) {
+                result = Double.POSITIVE_INFINITY;
+                throw new ArithmeticException("Case of Positive Infinity 1.0/0.0");
+            } else if (number1 <= -1 && number2 == 0) {
+                result = Double.NEGATIVE_INFINITY;
+                throw new ArithmeticException("Case of Negative Infinity -1.0/0.0");
+            } else {
+                result = number1 / number2;
+            }
+        } catch (ArithmeticException error) {
+            logger.error("Number cannot be divided by zero " + error.getLocalizedMessage());
+        } finally {
+            logger.info("Result of dividing is " + result);
+        }
+        return result;
+    }
 }
